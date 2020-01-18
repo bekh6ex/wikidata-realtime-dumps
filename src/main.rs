c435d4d8ed4558b5e034f3c5de18c9ec87730bd0
Aleksei Bekh-Ivanov <bekh6ex@users.noreply.github.com>
2020-01-19 00:58:26 +0100

    Extract warp_server module to file

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ mod events;
 mod get_entity;
 mod init;
 mod prelude;
+mod warp_server;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -111,93 +112,3 @@ fn init_logger() {
     log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
 }
 
-mod warp_server {
-    use std::collections::BTreeMap;
-    use std::convert::Infallible;
-    use std::net::SocketAddrV4;
-    use std::sync::Arc;
-
-    use actix::Addr;
-    use hyper::body::Bytes;
-    use hyper::{Body, StatusCode};
-    use warp::reply::Response;
-    use warp::*;
-
-    use crate::actor::archivarius::ArchivariusActor;
-    use crate::actor::GetDump;
-    use crate::prelude::EntityType;
-
-    pub async fn start(ar: &Arc<BTreeMap<EntityType, Addr<ArchivariusActor>>>) {
-        let hello = get_dump_route(ar);
-
-        let ws = warp::serve(hello).run("127.0.0.1:8080".parse::<SocketAddrV4>().unwrap());
-        ws.await
-    }
-
-    fn get_dump_route(
-        ar: &Arc<BTreeMap<EntityType, Addr<ArchivariusActor>>>,
-    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        warp::path!("dumps" / String)
-            .and(warp::get())
-            .and(with_actor(ar))
-            .and_then(get_dump_handler)
-    }
-
-    fn with_actor(
-        ar: &Arc<BTreeMap<EntityType, Addr<ArchivariusActor>>>,
-    ) -> impl Filter<
-        Extract = (Arc<BTreeMap<EntityType, Addr<ArchivariusActor>>>,),
-        Error = std::convert::Infallible,
-    > + Clone {
-        let ar = ar.clone();
-        warp::any().map(move || ar.clone())
-    }
-
-    const ENTITY_NAMES: [(&'static str, EntityType); 3] = [
-        ("properties", EntityType::Property),
-        ("items", EntityType::Item),
-        ("lexemes", EntityType::Lexeme),
-    ];
-
-    async fn get_dump_handler(
-        mut ty: String,
-        map: Arc<BTreeMap<EntityType, Addr<ArchivariusActor>>>,
-    ) -> Result<impl warp::Reply, Infallible> {
-        use futures::StreamExt;
-        // TODO: As long as we return chunks in order we can make it possible to return only certain
-        //       requested ranges of entities
-        let suffix = ".jsonl.gz";
-
-        if !ty.ends_with(suffix) {
-            return Ok(not_found());
-        }
-
-        ty.split_off(ty.len() - suffix.len());
-
-        let ty: Option<&EntityType> = ENTITY_NAMES
-            .into_iter()
-            .find(|(prefix, _)| **prefix == ty)
-            .map(|(_, entity_type)| entity_type);
-
-        if let Some(ty) = ty {
-            if let Some(ar) = map.get(ty) {
-                let result = ar
-                    .send(GetDump)
-                    .await
-                    .expect("Actor communication problem")
-                    .map(|b| Ok(b) as Result<Bytes, Infallible>);
-
-                return Ok(Response::new(Body::wrap_stream(result)));
-            }
-        }
-
-        Ok(not_found())
-    }
-
-    fn not_found() -> Response {
-        let mut r404 = Response::new(Body::from("Not found"));
-        *r404.status_mut() = StatusCode::NOT_FOUND;
-
-        r404
-    }
-}
