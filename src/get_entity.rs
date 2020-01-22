6539b63d455a9959c0008dea1f01f2f752081bfb
Aleksei Bekh-Ivanov <bekh6ex@users.noreply.github.com>
2020-01-22 23:28:40 +0100

    Adjust request timeouts

--- src/get_entity.rs
+++ src/get_entity.rs
@@ -31,8 +31,8 @@ pub async fn get_entity(client: Arc<Client>, id: EntityId) -> Option<GetEntityRe
 const INITIAL_TIMEOUT: u64 = 50;
 const MAX_TIMEOUT: u64 = 60000;
 static TIMEOUT: AtomicU64 = AtomicU64::new(INITIAL_TIMEOUT);
-const TIMEOUT_INCR: f32 = 1.3;
-const TIMEOUT_REDUCE: f32 = 0.9;
+const TIMEOUT_INCR: f32 = 1.1;
+const TIMEOUT_REDUCE: f32 = 0.99;
 const MAX_TRIES: u8 = 50;
 
 fn with_retries(
