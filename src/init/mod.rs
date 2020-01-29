edbc5113903b5a4602dc03ae536ecf9fb3e823bf
Aleksei Bekh-Ivanov <bekh6ex@users.noreply.github.com>
2020-01-30 00:24:32 +0100

    Use dump for item initialization

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -84,7 +84,7 @@ pub async fn init(
         entity_stream
     };
 
-    let final_stream = if true {
+    let final_stream = if max - min < 1_000_000 {
         raw_stream.left_stream()
     } else {
         stream_with_dump.right_stream()
