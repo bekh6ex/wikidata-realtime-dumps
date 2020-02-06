a16005459c75bdacfd6240dcc08545b21b8fde38
Aleksei Bekh-Ivanov <bekh6ex@users.noreply.github.com>
2020-02-06 23:37:43 +0100

    Refactor archivarius test

--- src/archive/test.rs
+++ src/archive/test.rs
@@ -1,44 +1,60 @@
-
-use actix_rt;
-use crate::archive::{Archivarius, Initialization, GetDump, GetDumpResult};
-use crate::prelude::{EntityType, SerializedEntity, RevisionId};
-use crate::archive::arbiter_pool::ArbiterPool;
-use actix::prelude::*;
-use crate::events::EventId;
-use futures::stream::*;
 use std::io::Read;
-use serde::{Deserialize, Serialize};
-use bytes::Bytes;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-#[actix_rt::test] #[test]
+use actix::prelude::*;
+use actix_rt;
+use bytes::Bytes;
+use futures::stream::*;
+use serde::{Deserialize, Serialize};
+
+use crate::archive::{Archivarius, GetDump, GetDumpResult, Initialization};
+use crate::archive::arbiter_pool::ArbiterPool;
+use crate::events::EventId;
+use crate::prelude::{EntityType, RevisionId, SerializedEntity};
+
+#[actix_rt::test]
+#[test]
 async fn just_initialized_archivarius_should_return_item_from_dump() {
+    with_temp_dir(|dir| async move {
+        let archivarius = item_archivarius(dir);
+
+        let mut revision = (1u64..).into_iter();
+        let mut next_rev = move || {
+            revision.next().unwrap()
+        };
+
+        let start_event_id = EventId::test(1);
+
+        archivarius.send(Initialization::Start(start_event_id)).await.unwrap().await;
+        archivarius.send(Initialization::UpdateEntity(item(1, next_rev()))).await.unwrap().await;
+        archivarius.send(Initialization::Finished).await.unwrap().await;
+
+        let dump_stream: GetDumpResult = archivarius.send(GetDump).await.unwrap();
+
+        let mut entities = to_entities(dump_stream).await;
+
+        assert_eq!(entities.len(), 1);
+        let entity = entities.pop().unwrap();
+        assert_eq!(entity.id, "Q1");
+        assert_eq!(entity.lastrevid, 1);
+    }).await
+}
+
+async fn with_temp_dir<Fut>(f: fn(String) -> Fut)
+    where Fut: Future<Output = ()>
+{
     let dir: u64 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
     let dir = format!("/tmp/wd-tests/{}", dir);
-    let archivarius = Archivarius::create(|ctx| {
+
+    f(dir.clone()).await;
+
+    std::fs::remove_dir_all(dir).unwrap();
+}
+
+fn item_archivarius(dir: String) -> Addr<Archivarius> {
+    Archivarius::create(|ctx| {
         Archivarius::new(&dir, EntityType::Item, pool(), ctx.address())
-    });
-
-    let mut revision = (1u64..).into_iter();
-
-    let mut next_rev = move || {
-        revision.next().unwrap()
-    };
-
-    let start_event_id = EventId::test(1);
-
-    archivarius.send(Initialization::Start(start_event_id)).await.unwrap().await;
-    archivarius.send(Initialization::UpdateEntity(item(1, next_rev()))).await.unwrap().await;
-    archivarius.send(Initialization::Finished).await.unwrap().await;
-
-    let dump_stream: GetDumpResult = archivarius.send(GetDump).await.unwrap();
-
-    let mut entities = to_entities(dump_stream).await;
-
-    assert_eq!(entities.len(), 1);
-    let entity = entities.pop().unwrap();
-    assert_eq!(entity.id, "Q1");
-    assert_eq!(entity.lastrevid, 1);
+    })
 }
 
 async fn to_entities(stream: GetDumpResult) -> Vec<Entity> {
